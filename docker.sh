3247a5eb9ae515bfe1d949b74dc2dc9fab7de1bb
Link <link@blockabc.com>
2021-03-04 16:26:30 +0800

    fix: fix bugs in docker.sh, support build different env by option

--- docker.sh
+++ docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Code path in container
-CODE_PATH="/code"
 # Docker image name
 DOCKER_IMAGE="jjy0/ckb-capsule-recipe-rust:2020-9-28"
 # Docker container name
@@ -11,30 +9,33 @@ CACHE_VOLUME="capsule-cache"
 
 function build() {
     local contract=$1
+    local is_release=$2
 
     if [ ! -d contracts/$contract ]; then
         echo "Contract ${contract} is not exists, please check for spelling errors."
         exit 1
     fi
 
-    # Build debug version
-    docker exec -it -w /code/contracts/$contract $DOCKER_CONTAINER bash -c \
-        "cargo build --target riscv64imac-unknown-none-elf && ckb-binary-patcher -i /code/target/riscv64imac-unknown-none-elf/debug/${contract} -o /code/target/riscv64imac-unknown-none-elf/debug/${contract}"
-    docker exec -it -w /code $DOCKER_CONTAINER bash -c \
-        "cp /code/target/riscv64imac-unknown-none-elf/debug/${contract} /code/build/debug"
-
-    # Build release version
-#    docker exec -it -w /code/contracts/$contract $DOCKER_CONTAINER bash -c \
-#        "RUSTFLAGS=\"-Z pre-link-arg=-zseparate-code -Z pre-link-arg=-zseparate-loadable-segments\" cargo build --release --target riscv64imac-unknown-none-elf && ckb-binary-patcher -i /code/target/riscv64imac-unknown-none-elf/release/${contract} -o /code/target/riscv64imac-unknown-none-elf/release/${contract}"
-#    docker exec -it -w /code $DOCKER_CONTAINER bash -c \
-#        "cp /code/target/riscv64imac-unknown-none-elf/release/${contract} /code/build/release"
+    if [[ is_release == "--release" ]]; then
+        # Build release version
+        docker exec -it -w /code/contracts/$contract $DOCKER_CONTAINER bash -c \
+            "RUSTFLAGS=\"-Z pre-link-arg=-zseparate-code -Z pre-link-arg=-zseparate-loadable-segments\" cargo build --release --target riscv64imac-unknown-none-elf && ckb-binary-patcher -i /code/target/riscv64imac-unknown-none-elf/release/${contract} -o /code/target/riscv64imac-unknown-none-elf/release/${contract}"
+        docker exec -it -w /code $DOCKER_CONTAINER bash -c \
+            "cp /code/target/riscv64imac-unknown-none-elf/release/${contract} /code/build/release/"
+    else
+        # Build debug version
+        docker exec -it -w /code/contracts/$contract $DOCKER_CONTAINER bash -c \
+            "cargo build --target riscv64imac-unknown-none-elf && ckb-binary-patcher -i /code/target/riscv64imac-unknown-none-elf/debug/${contract} -o /code/target/riscv64imac-unknown-none-elf/debug/${contract}"
+        docker exec -it -w /code $DOCKER_CONTAINER bash -c \
+            "cp /code/target/riscv64imac-unknown-none-elf/debug/${contract} /code/build/debug/"
+    fi
 }
 
 function build_all() {
     dirs=$(ls -a contracts)
     for contract in $dirs; do
         if [[ $contract != "." && $contract != ".." && -d contracts/$contract ]]; then
-            build $contract
+            build $contract $1
         fi
     done
 }
@@ -44,17 +45,27 @@ start)
     docker run -it --rm \
         --name $DOCKER_CONTAINER \
         -v ${PWD}:/code \
-        -v /Users/xieaolin/Documents/blockabc/das-types:/das-types \
+        -v ${PWD}/das-types:/das-types \
         -v $CACHE_VOLUME:/root/.cargo \
         -e RUSTFLAGS="-Z pre-link-arg=-zseparate-code -Z pre-link-arg=-zseparate-loadable-segments" \
         -e CAPSULE_TEST_ENV=debug \
         $DOCKER_IMAGE bin/bash
     ;;
 build)
-    if [ -z $2 ];then
-        build_all
+    if [[ $2 == "--release" || $3 == "--release" ]]; then
+        if [[ ! -d ./build/release ]]; then
+            mkdir -p ./build/release
+        fi
     else
-        build $2
+        if [[ ! -d ./build/debug ]]; then
+            mkdir -p ./build/debug
+        fi
+    fi
+
+    if [[ -z $2 || $2 == "--release" ]];then
+        build_all $2
+    else
+        build $2 $3
     fi
     ;;
 test)
