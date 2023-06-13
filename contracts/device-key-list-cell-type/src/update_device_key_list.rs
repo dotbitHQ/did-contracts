b40e4567b50ac63112354e45556bd9069746df8b
Louis Yu <louis@did.id>
2023-06-13 16:32:48 +0800

    fix: fixed update

--- contracts/device-key-list-cell-type/src/update_device_key_list.rs
+++ contracts/device-key-list-cell-type/src/update_device_key_list.rs
@@ -1,18 +1,17 @@
 use alloc::boxed::Box;
-
 use alloc::vec::Vec;
+
 use ckb_std::ckb_types::packed::Script;
 use das_core::constants::das_lock;
 use das_core::error::ScriptError;
 use das_core::{assert, code_to_error, debug};
-use das_types::packed::{DeviceKeyList, DeviceKeyListCellData, DeviceKey};
 use das_types::convert::*;
+use das_types::packed::{DeviceKey, DeviceKeyList, DeviceKeyListCellData};
 use molecule::prelude::Entity;
 
 use crate::error::ErrorCode;
 use crate::helpers::{Comparable, ToNum};
 use crate::traits::{Action, FSMContract, Rule};
-
 
 pub fn action() -> Action {
     let mut update_action = Action::new("update_device_key_list");
@@ -30,7 +29,9 @@ pub fn action() -> Action {
 
     update_action.add_verification(Rule::new("Verify capacity change", |contract| {
         assert!(
-            contract.input_inner_cells[0].capacity().to_num() - contract.output_inner_cells[0].capacity().to_num() < 10000,
+            i64::try_from(contract.input_inner_cells[0].capacity().to_num()).unwrap()
+                - i64::try_from(contract.output_inner_cells[0].capacity().to_num()).unwrap()
+                < 10000,
             ErrorCode::CapacityReduceTooMuch,
             "Capacity change is too much"
         );
@@ -39,7 +40,7 @@ pub fn action() -> Action {
 
     update_action.add_verification(Rule::new("Verify lock consistent", |contract| {
         assert!(
-            contract.input_inner_cells[0].lock().as_slice() == contract.output_inner_cells[0].as_slice(),
+            contract.input_inner_cells[0].lock().as_slice() == contract.output_inner_cells[0].lock().as_slice(),
             ErrorCode::InvalidLock,
             "Lock should not change"
         );
@@ -48,14 +49,15 @@ pub fn action() -> Action {
 
     update_action.add_verification(Rule::new("Verify key list structure", |contract| {
         let key_list_in_input = contract.get_cell_witness::<DeviceKeyListCellData>(&contract.input_inner_cells[0])?;
-        let key_list_in_output = contract.get_cell_witness::<DeviceKeyListCellData>(&contract.input_inner_cells[0])?;
+        let key_list_in_output = contract.get_cell_witness::<DeviceKeyListCellData>(&contract.output_inner_cells[0])?;
         das_core::assert!(
             key_list_in_output.keys().item_count() > 0 && key_list_in_output.keys().item_count() < 11,
             ErrorCode::UpdateParamsInvalid,
             "The key list length should be from 1 to 10"
         );
 
-        let len_diff: i32 = key_list_in_input.keys().item_count() as i32 - key_list_in_output.keys().item_count() as i32;
+        let len_diff: i64 =
+            i64::try_from(key_list_in_output.keys().item_count()).unwrap() - i64::try_from(key_list_in_input.keys().item_count()).unwrap();
         das_core::assert!(
             len_diff == 1 || len_diff == -1,
             ErrorCode::KeyListNumberIncorrect,
@@ -78,10 +80,16 @@ pub fn action() -> Action {
             }
             -1 => {
                 debug!("update_device_key_list: remove key");
-                let keys_in_input: alloc::collections::BTreeSet<Comparable<DeviceKey>> =
-                    key_list_in_input.keys().into_iter().map(|key| Comparable(key)).collect();
-                let keys_in_output: alloc::collections::BTreeSet<Comparable<DeviceKey>> =
-                    key_list_in_output.keys().into_iter().map(|key| Comparable(key)).collect();
+                let keys_in_input: alloc::collections::BTreeSet<Comparable<DeviceKey>> = key_list_in_input
+                    .keys()
+                    .into_iter()
+                    .map(|key| Comparable(key))
+                    .collect();
+                let keys_in_output: alloc::collections::BTreeSet<Comparable<DeviceKey>> = key_list_in_output
+                    .keys()
+                    .into_iter()
+                    .map(|key| Comparable(key))
+                    .collect();
                 das_core::assert!(
                     keys_in_input.is_superset(&keys_in_output),
                     ErrorCode::UpdateParamsInvalid,
