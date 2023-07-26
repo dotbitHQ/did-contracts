e06d87650bff52d013437f71c0bc214b616ab765
Louis Yu <louis@did.id>
2023-07-26 16:19:25 +0800

    fix: fixed generating cell_deps BTree

--- libs/das-core/src/witness_parser/device_key_list.rs
+++ libs/das-core/src/witness_parser/device_key_list.rs
@@ -2,48 +2,23 @@ use alloc::collections::BTreeMap;
 use core::slice::SlicePattern;
 
 use ckb_std::ckb_constants::Source;
-use ckb_std::high_level::{load_cell_lock, load_cell_type_hash, QueryIter};
-use ckb_std::syscalls::SysError;
+use ckb_std::high_level::{load_cell_lock, load_cell_type, QueryIter};
 use molecule::bytes::Bytes;
+use molecule::prelude::Entity;
 
 use crate::constants::device_key_list_cell_type;
 
 pub fn get_device_key_list_cell_deps() -> BTreeMap<[u8; 32], Bytes> {
-    // let mut i = 0;
-    // let mut cell_deps: BTreeMap<[u8; 32], Bytes> = Default::default();
-
-    // loop {
-    //     let type_hash = match load_cell_type_hash(i, Source::CellDep) {
-    //         Ok(res) => res,
-    //         Err(SysError::IndexOutOfBound) => break,
-    //         e => e.unwrap(),
-    //     };
-
-    //     if let Some(type_hash) = type_hash {
-    //         if type_hash == device_key_list_cell_type().code_hash().raw_data().as_slice() {
-    //             let type_hash = load_cell_type_hash(i, Source::CellDep).unwrap().unwrap();
-    //             debug!("type_hash: {:?}", type_hash);
-    //             let lock = load_cell_lock(i, Source::CellDep).unwrap();
-    //             let mut buf = [0; 32];
-    //             let _ = ckb_std::syscalls::load_cell_data(&mut buf, 0, i, Source::CellDep);
-    //             debug!("buf: {:?}", buf);
-    //             debug!("lock args: {:?}", lock.args().raw_data());
-    //             cell_deps.insert(buf, lock.args().raw_data());
-    //         }
-    //     }
-        
-    //     i += 1;
-    // }
     let cell_deps = QueryIter::new(
         |index, source| {
-            let res = load_cell_type_hash(index, source)?;
-            Ok(res.map(|hash| (index, hash)))
+            let res = load_cell_type(index, source)?;
+            Ok(res.map(|script| (index, script.code_hash())))
         },
         Source::CellDep,
     )
     .filter_map(|res| {
         res.and_then(|(index, hash)| {
-            if hash == device_key_list_cell_type().code_hash().raw_data().as_slice() {
+            if hash.as_slice() == device_key_list_cell_type().code_hash().as_slice() {
                 let lock = load_cell_lock(index, Source::CellDep).unwrap();
                 let mut buf: [u8; 32] = [0; 32];
                 let _ = ckb_std::syscalls::load_cell_data(&mut buf, 0, index, Source::CellDep);
@@ -53,15 +28,6 @@ pub fn get_device_key_list_cell_deps() -> BTreeMap<[u8; 32], Bytes> {
             }
         })
     })
-    // .map(|(index, _hash)| {
-    //     let lock = load_cell_lock(index + 1, Source::CellDep).unwrap();
-    //     let mut buf: [u8; 32] = [0; 32];
-    //     let _ = ckb_std::syscalls::load_cell_data(&mut buf, 0, index + 1, Source::CellDep);
-    //     debug!("index: {}", index + 1);
-    //     debug!("buf: {:?}", buf);
-    //     debug!("lock_arg: {:?}", lock.args().raw_data());
-    //     (buf, lock.args().raw_data())
-    // })
     .collect();
 
     cell_deps
